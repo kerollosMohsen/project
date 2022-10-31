b3141575e7a067aa80087bf0dc5b3feff76509dc
kerollosMohsen <115957504+kerollosMohsen@users.noreply.github.com>
2022-10-31 22:42:08 +0200

    Add files via upload

---
+++ games/src/data.ts
@@ -0,0 +1,70 @@
+// import { Product } from "./app/shared/models/products";
+
+
+// export const product_sample:Product[]=[
+//     {
+//         _id:'1',
+//         title: 'Sony PlayStaion 5',
+//         price: 18999,
+//         favorite: false,
+//         stars: 4.5,
+//         imageUrl: 'assets/ps5.jpg',
+//         categories: ['Game Console'],
+//         des:"(PlayStation 5) The latest Sony PlayStation introduced in November 2020"
+//       },
+//       {
+//         id:'2',
+//         title: 'Sony Playstaion 4',
+//         price: 8499,
+//         favorite: true,
+//         stars: 4.7,
+//         imageUrl: 'assets/ps4.webp',
+//         categories: ['Game Console'],
+//       },
+//       {
+//         id:'3',
+//         title: 'XBOX One X',
+//         price: 16649,
+//         favorite: false,
+//         stars: 3.5,
+//         imageUrl: 'assets/xboxonex.webp',
+//         categories: ['Game Console'],
+//       },
+//       {
+//         id:'4',
+//         title: 'XBOX One S',
+//         price: 8999,
+//         favorite: true,
+//         stars: 3.3,
+//         imageUrl: 'assets/xboxones.webp',
+//         categories: ['Game Console'],
+//       },
+//       {
+//         id:'5',
+//         title: 'XBOX One S controller',
+//         price: 1200,
+//         favorite: false,
+//         stars: 3.0,
+//         imageUrl: 'assets/xbosc.jpg',
+//         categories: ['accessories', 'Game Console','controller'],
+//       },
+//       {
+//         id:'6',
+//         title: 'XBOX One X controller',
+//         price: 1400,
+//         favorite: false,
+//         stars: 4.0,
+//         imageUrl: 'assets/xboxc.jpg',
+//         categories: ['accessories', 'Game Console','controller'],
+//       },
+//       {
+//         id:'7',
+//         title: 'XBOX One X controller',
+//         price: 9,
+//         favorite: false,
+//         stars: 4.0,
+//         imageUrl: 'assets/xboxc.jpg',
+//         categories: ['accessories', 'Game Console','controller'],
+      
+//       }
+// ]
